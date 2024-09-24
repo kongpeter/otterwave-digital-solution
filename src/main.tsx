94b477495a1b6a14ac6df90c5cfc8d5595c87f2b
kongpeter <xjtlu2014@outlook.com>
2024-09-24 17:10:06 +1000

    feat: project init

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import './index.css';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const root = createRoot(document.getElementById('root') as HTMLDivElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
